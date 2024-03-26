73d14b1ee652d7c5c2ab8dfe7e525d01ef58579a
Abdul Basith <ssabdulbasith@gmail.com>
2024-03-26 10:16:43 +0300

    Finish JS Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    reversedText = "";
+    while (text!="") {
+        reversedText += text.slice(-1);
+        text = text.slice(0, -1);
+    }
+    return reversedText;
 };
 
 // Do not edit below this line

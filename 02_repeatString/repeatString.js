dacce59eeb7545ea109099c80f5624de99de24f3
Abdul Basith <ssabdulbasith@gmail.com>
2024-03-26 09:54:51 +0300

    Finish JS Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let finalString = "";
+    if (num <0) return "ERROR";
+    for (i=0; i<num; i++) {
+        finalString += text;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
